3bf808d6bc63033662ed1a6348e20c9fe4bfb668
ABU SAYED <farabisayed6099@gmail.com>
2025-12-12 15:46:20 +0600

    js-func-programmin

---
+++ javascript/functional-programming/even-number-find.js
@@ -0,0 +1,5 @@
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const evenNumber = numbers.filter((num) => num % 2 === 0);
+
+console.log(evenNumber); // Output: [2, 4, 6]
---
+++ javascript/functional-programming/multiple-number.js
@@ -0,0 +1,5 @@
+const numbers = [1, 3, 5, 7, 8, 9, 10];
+
+const multipleByTwo = numbers.map((num) => num * 2);
+
+console.log(multipleByTwo);
