a9fdbac1e032aeb0d380b2df2fa0542d52938710
ABU SAYED <farabisayed6099@gmail.com>
2025-12-12 21:48:55 +0600

    arrayMultiply in multiple-number

--- javascript/functional-programming/multiple-number.js
+++ javascript/functional-programming/multiple-number.js
@@ -3,3 +3,8 @@ const numbers = [1, 3, 5, 7, 8, 9, 10];
 const multipleByTwo = numbers.map((num) => num * 2);
 
 console.log(multipleByTwo);
+
+const arrayMultiply = (arr, n) => arr.map((item) => item * n);
+
+const result = arrayMultiply(numbers, 5);
+console.log(result);
