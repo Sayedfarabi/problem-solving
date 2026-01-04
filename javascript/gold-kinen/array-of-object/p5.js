38058f12d56ac8fedba6699a44032ee653340070
ABU SAYED <farabisayed6099@gmail.com>
2026-01-04 23:20:33 +0600

    p7

--- javascript/gold-kinen/array-of-object/p5.js
+++ javascript/gold-kinen/array-of-object/p5.js
@@ -3,11 +3,12 @@ const subscriptions = [
   { userId: 1, plan: "basic", expiresAt: "2024-12-31" },
   { userId: 2, plan: "pro", expiresAt: "2025-02-01" },
   { userId: 3, plan: "pre", expiresAt: "2025-02-01" },
-  { userId: 4, plan: "exclusive", expiresAt: "2026-01-01" },
+  { userId: 4, plan: "pro", expiresAt: "2026-02-01" },
 ];
 
 const findExpiredSubscription = (subscriptions) => {
   const now = Date.now();
+  console.log(now);
 
   return subscriptions.filter(({ expiresAt }) => {
     return new Date(expiresAt).getTime() < now;
---
+++ javascript/gold-kinen/array-of-object/p7.js
@@ -0,0 +1,84 @@
+// Let's say we have a list of expenses
+// We want to get a category wise report of expenses.
+// Let's write a function that takes an array of expense objects
+// and returns an array of objects which summarizes the expenses
+// based on category.
+
+// Example input:
+
+const expenses = [
+  {
+    category: "food",
+    amount: 200,
+    date: "2024-01-01",
+  },
+  {
+    category: "travel",
+    amount: 200,
+    date: "2024-02-01",
+  },
+  {
+    category: "travel",
+    amount: 130,
+    date: "2024-03-20",
+  },
+  {
+    category: "food",
+    amount: 150,
+    date: "2024-03-22",
+  },
+  {
+    category: "food",
+    amount: 100,
+    date: "2024-03-23",
+  },
+];
+// Example output:
+/**
+ * 
+[
+  {
+    "category": "food",
+    "total": 450
+  },
+  {
+    "category": "travel",
+    "total": 330
+  }
+]
+*/
+
+// {
+//   "food": 450,
+//   "travel": 330
+// }
+
+const generateGroup = (array) => {
+  const grouped = array.reduce((acc, curr) => {
+    const category = curr.category;
+    if (!acc[category]) {
+      acc[category] = curr.amount;
+    } else {
+      acc[category] = acc[category] + curr.amount;
+    }
+    return acc;
+  }, {});
+
+  console.log({ grouped });
+
+  const categories = Object.keys(grouped); // [food, travel]
+
+  const result = [];
+  for (const category of categories) {
+    const amount = grouped[category];
+    result.push({
+      category,
+      amount,
+    });
+  }
+
+  return result;
+};
+
+const result = generateGroup(expenses);
+// console.log(result);
